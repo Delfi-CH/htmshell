676e180b04c8f1d69a84d2618e7fb2bb60c19761
Delfi-CH <delfi@delfi.dev>
2026-05-21 18:47:27 +0200

    feat: update express to use parameters

--- express-htmshell/example.js
+++ express-htmshell/example.js
@@ -1,23 +1,12 @@
 import express from "express";
 import { createServer } from "http";
-import { WebSocketServer } from "ws";
-import { expressHtmshell } from "express-htmshell";
+import { setupHtmshell } from "./index.js";
 
 const app = express();
 const server = createServer(app);
 
-const wss = new WebSocketServer({ noServer: true });
+setupHtmshell(server, "/shell");
 
-expressHtmshell("bash")(wss);
-
-server.on("upgrade", (req, socket, head) => {
-    if (req.url.startsWith("/shell")) {
-        wss.handleUpgrade(req, socket, head, (ws) => {
-            wss.emit("connection", ws, req);
-        });
-    } else {
-        socket.destroy();
-    }
+server.listen(3000, () => {
+    console.log("listening on port 3000");
 });
-
-server.listen(3000, () => console.log("listening on port 3000"));
--- express-htmshell/index.js
+++ express-htmshell/index.js
@@ -1,29 +1,75 @@
 import * as os from "node:os";
 import * as pty from "node-pty";
 import process from "node:process";
+import { WebSocketServer } from "ws";
+import { URL } from "node:url";
 
-export function expressHtmshell(shell) {
-    return (wss) => {
-        wss.on("connection", (ws, req) => {
-            const bash = pty.spawn(shell, [], {
-                name: "xterm-256color",
-                cols: 80,
-                rows: 24,
-                cwd: os.homedir(),
-                env: process.env
-            });
+export function setupHtmshell(server, path) {
+    const wss = new WebSocketServer({
+        noServer: true
+    });
 
-            ws.on("message", (data) => {
-                bash.write(data.toString());
-            });
+    server.on("upgrade", (req, socket, head) => {
+        const url = new URL(req.url, `http://${req.headers.host}`);
 
-            bash.onData((data) => {
-                ws.send(data);
-            });
+        if (url.pathname !== path) {
+            socket.destroy();
+            return;
+        }
 
-            bash.onExit(() => {
-                ws.close(1000);
-            });
+        const shell = url.searchParams.get("shell") || "bash";
+        const rows = Number(url.searchParams.get("rows")) || 24;
+        const cols = Number(url.searchParams.get("cols")) || 80;
+
+        wss.handleUpgrade(req, socket, head, (ws) => {
+            ws.shell = shell;
+            ws.rows = rows;
+            ws.cols = cols;
+
+            wss.emit("connection", ws, req);
         });
-    };
+    });
+
+    wss.on("connection", (ws) => {
+        spawnShell(ws);
+    });
+};
+
+export function spawnShell(ws) {
+    const term = pty.spawn(ws.shell, [], {
+        name: "xterm-256color",
+        cols: ws.cols,
+        rows: ws.rows,
+        cwd: os.homedir(),
+        env: process.env
+    });
+
+    console.log(`started shell: ${ws.shell}`);
+
+    ws.on("message", (data) => {
+        try {
+            const parsed = JSON.parse(data);
+
+            if (parsed.type === "resize") {
+                term.resize(parsed.cols, parsed.rows);
+                return;
+            }
+        } catch {}
+
+        term.write(data.toString());
+    });
+
+    term.onData((data) => {
+        if (ws.readyState === ws.OPEN) {
+            ws.send(data);
+        }
+    });
+
+    ws.on("close", () => {
+        term.kill();
+    });
+
+    term.onExit(() => {
+        ws.close();
+    });
 }
